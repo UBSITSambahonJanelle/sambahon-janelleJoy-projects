7366af0597f9e387b08e04cee916bbadf596340f
Janelle Jjoy Sambahon <20244215@s.ubaguio.edu>
2026-02-19 19:10:12 +0800

    Implement property and event binding in Angular components

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { UserInteractionComponent } from './user-interaction/user-interaction.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HomeComponent, UserInteractionComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-app';
+}
---
+++ src/app/home/home.component.spec.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  appTitle: string = 'Welcome to Angular 20';
+  userImageUrl: string = 'https://www.w3schools.com/howto/img_avatar.png';
+  isHighlighted: boolean = true;
+  currentDate: Date = new Date();
+}
